3c22b7188fd3c2481efee1b9d7fc046bddf8b91a
tick0x <71328246+tick0x@users.noreply.github.com>
2021-02-19 16:51:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+// navbar
+const menu=document.querySelector(".menu")
+const navbar=document.querySelector(".navbar")
+
+menu.addEventListener('click', ()=> {
+    navbar.classList.toggle('change')
+    menu.classList.toggle('change')
+})
+
+// section 2 video
+
+const video = document.querySelector(".video")
+const btn = document.querySelector(".buttons i")
+const bar= document.querySelector(".video-bar")
+
+const PlayPause = ()=>{
+    if(video.paused){
+        video.play()
+    btn.className="far fa-pause-circle"
+    video.style.opacity=".7"
+    }else{
+        video.pause()
+    btn.className="far fa-play-circle"
+    video.style.opacity=".3"
+    }
+    
+}
+
+btn.addEventListener("click", ()=> {
+    PlayPause()
+})
+
+video.addEventListener("timeupdate", ()=>{
+    const barWidth=video.currentTime/video.duration
+    bar.style.width = `${barWidth*100}%`
+    if(video.ended){
+        btn.className='far fa-play-circle'
+        video.style.opacity=0.3
+    }
+})
+
+
+
+// section 3 pricing cards
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
